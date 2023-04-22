d540687f012aefaf302d79d7cc0bfcb20515f2a7
NotPerr <vovo7292@gmail.com>
2023-04-23 00:11:06 +0800

    add loader

---
+++ index.js
@@ -0,0 +1,8 @@
+
+setTimeout(() => {
+    const box = document.getElementById('loader_container');
+  
+    // 👇️ hides element (still takes up space on the page)
+    box.style.display = 'none';
+  }, 2000);
+
