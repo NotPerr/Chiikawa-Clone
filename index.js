5788f559b42c1efd889b8d0aeed501dcac322be1
NotPerr <vovo7292@gmail.com>
2023-04-24 21:36:21 +0800

    add banner and twitter link

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 setTimeout(() => {
     const box = document.getElementById('loader_container');
+    const main = document.getElementById('main');
   
     // 👇️ hides element (still takes up space on the page)
     box.style.display = 'none';
+    main.style.display = 'block';
   }, 2000);
 
