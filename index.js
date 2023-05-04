2336f5a50cd2cade57a868d69a6f37e1c34ba3ff
NotPerr <vovo7292@gmail.com>
2023-05-04 15:15:31 +0800

    add scroll-top btn

--- index.js
+++ index.js
@@ -113,3 +113,13 @@ if(openMenu) {
     closeBtnMenu.classList.add('show');
   })
 }
+
+
+//scroll top
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-top')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
